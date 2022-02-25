60113a215691ce5814bf653bf8ed82c7603a52a0
Michael Notych <notych2812@gmail.com>
2022-02-25 15:58:31 +0300

    Fixed bugs and finished stats.html

--- src/js/app.js
+++ src/js/app.js
@@ -4,3 +4,19 @@ myFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from "swiper";
 
 const swiper = new Swiper();
+
+$('#demo').daterangepicker({
+    ranges: {
+        'Today': [moment(), moment()],
+        'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+        'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+        'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+        'This Month': [moment().startOf('month'), moment().endOf('month')],
+        'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+    },
+    "alwaysShowCalendars": true,
+    "startDate": "02/17/2022",
+    "endDate": "02/23/2022"
+}, function(start, end, label) {
+  console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD') + ' (predefined range: ' + label + ')');
+});
